0a4bc0150039d001bcac6d5eb6e563bff2216ea8
Qing Fu <zz5840@qq.com>
2021-07-04 19:09:54 +0800

    fix: 🐛 Fix the built time display error

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,4 +1,5 @@
 export const VERSION = process.env.VERSION;
 export const COMMIT = process.env.COMMIT;
 export const BUILT_TIME = parseInt(process.env.BUILT_TIME as string);
+export const BUILT_TIME = parseInt(process.env.BUILT_TIME as string) * 1000;
 
